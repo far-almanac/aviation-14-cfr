750f8b6fbae73c32410d413e5c0f7cfc2402a64f
Michael McCracken <michael.mccracken172@gmail.com>
2018-03-04 23:50:08 -0500

    it is collecting now woo

--- lib/aviation_14_CFR.rb
+++ lib/aviation_14_CFR.rb
@@ -6,7 +6,36 @@ module Aviation14CFR
     File.read("data/CFR-2017-title14-vol#{vol}.xml")
   end
 
-  def self.data(vol=1)
-    Nokogiri::XML(self.file_contents(vol))
+  def self.nodes
+    nodes = []
+    self.volumes.each { |vol|
+      Nokogiri::XML(self.file_contents(vol)).traverse do |n|
+        nodes.push(n)
+      end
+    }
+    nodes
   end
+
+  def self.foo
+    #x.xpath("/CFRDOC/TITLE/CHAPTER/SUBCHAP[1]/PART[2]/SUBPART[1]/SECTION[37]").children[1].children[0].text.start_with?('§ 61')
+    self.nodes.collect { |node|
+      begin
+        node if node.children[1].children[0].text.start_with?('§ 91')
+      rescue
+      end
+    }.reject { |c| c.nil? }
+  end
+
+  def self.list_regs(arr)
+    arr.each do |x|
+      puts x.children[1].text
+    end
+  end
+
+  def self.volumes
+    Dir["data/*.xml"].collect { |file|
+      file.match(/(CFR-2017-title14-vol)([0-9]{1,5})*(.xml)*/)[2]
+    }.sort
+  end
+
 end
